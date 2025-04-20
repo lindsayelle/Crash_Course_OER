f1deb6968e8f940f92b2eb4b86d5a0d0f1855abd
lindsayelle <lindsayellee@gmail.com>
2025-04-20 23:14:15 +0800

    Added footer showing copyright

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ToastProvider from "@/components/ToastProvider";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
       <body suppressHydrationWarning className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
         <ToastProvider />
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+      <footer className="footer">
+       <a href="https://crash-course-oer.vercel.app/">OER Crash Course Platform</a> © 2025 by <a href="https://www.linkedin.com/in/lindsayecsong/">Lindsay Elle Chiara Song</a> is licensed under <a href="https://creativecommons.org/licenses/by/4.0/?ref=chooser-v1">Creative Commons Attribution 4.0 International</a> 
+      </footer>
+    );
+  }
